21354aedbaae48378bb9b4698a37f1dbd00c9f19
shubham2270 <shubham2270@gmail.com>
2021-08-01 13:16:32 +0530

    update config js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ module.exports = {
       input: {
         main: resolve(__dirname, "./index.html"),
         experiment3: resolve(__dirname, "./experiments/experiment3/index.html"),
-        // exp3: resolve(__dirname, "./experiments/experiment3/exp3.js"),
+        experiment2: resolve(__dirname, "./experiments/experiment2/index.html"),
+        experiment1: resolve(__dirname, "./experiments/experiment1/index.html"),
       },
     },
   },
